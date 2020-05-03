7a67ef4af300d0421be4f8688e12392bea011431
badeaadi <badea_adi1999@yahoo.com>
2020-05-03 20:08:15 +0300

    Final

--- app/src/main/java/com/example/kars/LoginActivity.java
+++ app/src/main/java/com/example/kars/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.kars;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,27 +8,80 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
+    private EditText mEditTextEmail;
+    private EditText mEditTextPassword;
+    private static final String TAG = "EmailPassword";
+    private AdView mAdView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
+        mEditTextEmail = findViewById(R.id.username);
+        mEditTextPassword = findViewById(R.id.password);
         configureButton();
         configureLoginButton();
 
         mAuth = FirebaseAuth.getInstance();
+
+
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
     }
 
     private void configureLoginButton() {
-        Button mButtonRegister = findViewById(R.id.logLogin);
+
+        Button mButtonLogin = findViewById(R.id.logLogin);
+
+
+        mButtonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String email = mEditTextEmail.getText().toString();
+                String password = mEditTextPassword.getText().toString();
+
+                mAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Log.d(TAG, "signInWithEmail:success");
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    updateUI(user);
+                                } else {
+                                    Log.w(TAG, "signInWithEmail:failure", task.getException());
+                                    Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                    updateUI(null);
+                                }
+                            }
+                        });
+            }
+        });
     }
 
     @Override
@@ -39,7 +93,8 @@ public class LoginActivity extends AppCompatActivity {
 
     private void updateUI (FirebaseUser currentUser){
 
-        setContentView(R.layout.activity_login);
+        Intent myIntent = new Intent(LoginActivity.this, SaveCar.class);
+        startActivity(myIntent);
     }
 
 
